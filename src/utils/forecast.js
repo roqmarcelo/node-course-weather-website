e8ed426b30f857d0fbe712f32eeead1bbb25aeda
Roque Santos <roquesantosdev@gmail.com>
2021-04-03 13:07:48 -0300

    Added Humidity information to the forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,8 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            const { temperature, feelslike, weather_descriptions } = body.current
-            callback(undefined, `${weather_descriptions[0]}. It is currently ${temperature} degrees out. It feels like ${feelslike} degrees out.`)
+            const { temperature, feelslike, weather_descriptions, humidity } = body.current
+            callback(undefined, `${weather_descriptions[0]}. It is currently ${temperature} degrees out. It feels like ${feelslike} degrees out. The humidity is ${humidity} percent.`)
         }
     })
 }
